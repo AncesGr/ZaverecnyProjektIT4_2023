f62b51242773e542d79aeb4ab56cbd34b03886de
Anna Greplová <Greplova.Anna@stud.vassboskovice.cz>
2023-04-02 19:19:48 +0200

    formulare WorkHours

---
+++ Zaverecny_projekt_Greplova/Zaverecny_projekt_Greplova/AdminWorkHoursEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zaverecny_projekt_Greplova
+{
+    public partial class AdminWorkHoursEdit : Form
+    {
+        public AdminWorkHoursEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
